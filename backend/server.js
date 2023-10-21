008456fe279fba7dbcecbf804bf4514242afcfc4
Osiris8 <miganosiris8@gmaaail.com>
2023-10-21 18:57:42 +0200

    update cors options in server.js

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,16 @@ const app = express();
 
 // Autorisez les requêtes provenant de l'origine http://localhost:3000
 
+// Configuration CORS
 const corsOptions = {
-  origin: ["https://source-frontend-nine.vercel.app"],
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true, // Permet d'envoyer les cookies et les headers d'authentification
+  origin: "https://source-frontend-nine.vercel.app", // Autorise uniquement les requêtes en provenance de https://example.com (vous pouvez utiliser une liste de domaines ici)
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Méthodes HTTP autorisées
+  credentials: true, // Autorise les cookies et les informations d'authentification à être partagés
+  allowedHeaders: "Content-Type,Authorization,X-Requested-With", // Entêtes HTTP autorisées
+  exposedHeaders: "Content-Range,X-Content-Range", // Entêtes HTTP exposées
+  preflightContinue: false, // Contrôle des requêtes OPTIONS
+  optionsSuccessStatus: 204, // Code de réponse pour les requêtes OPTIONS
+  maxAge: 3600, // Durée en secondes pendant laquelle les résultats des pré-vérifications (preflight) peuvent être mis en cache
 };
 
 app.use(cors(corsOptions));
